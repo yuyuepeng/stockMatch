37f5b7ac620c2410c2018e32a231ec1d977c1e1a
玉岳鹏 <yuyuepeng@hexindai.com>
2019-08-12 18:03:07 +0800

    no message

---
+++ stockMatch/stockMatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  stockMatch
+//
+//  Created by 玉岳鹏 on 2019/8/12.
+//  Copyright © 2019 玉岳鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
